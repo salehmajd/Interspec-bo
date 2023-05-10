90f117c3acf1c50e48563d99cf02dc6bea3c41d4
Majd Saleh <majd.sa@gmail.com>
2023-05-10 20:38:47 +0200

    go live

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from backoffice.views import HomePageView, SearchResultsView
+from backoffice.views import HomePageView, SearchResultsView, detail
 
 urlpatterns = [
     path("", HomePageView, name="home"),
     path("search/", SearchResultsView.as_view(), name="search_results"),
+    path("detail/<int:emprunt_id>/", detail, name="detail")
 ]
--- backoffice/views.py
+++ backoffice/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView
 
 from backoffice.models import Adherent, emprunt
@@ -22,3 +22,9 @@ class SearchResultsView(ListView):
         query = self.request.GET.get("q")
         object_list = Adherent.objects.filter(Q(nom__icontains=query) | Q(prenom__icontains=query))
         return object_list
+
+def detail(request, emprunt_id):
+    emp = get_object_or_404(emprunt, pk=emprunt_id)
+    context = {'emp': emp}
+    template_name = "emprunt_detail.html"
+    return render(request, template_name, context)
